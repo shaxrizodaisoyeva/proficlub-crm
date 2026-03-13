547ceb18387909b73a430f01d985e730b81b744c
shaxrizodaisoyeva <121097473+shaxrizodaisoyeva@users.noreply.github.com>
2026-03-13 14:40:07 +0500

    main.jsx

--- proficlub-crm/src/main.jsx
+++ proficlub-crm/src/main.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import EmployeePortal from './EmployeePortal.jsx'
+
+const path = window.location.pathname
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {path.startsWith('/portal') ? <EmployeePortal /> : <App />}
   </React.StrictMode>
 )

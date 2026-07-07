8e8dd30543e4b60c93fc83fed5fa7faf631eba31
shaxrizodaisoyeva <121097473+shaxrizodaisoyeva@users.noreply.github.com>
2026-07-07 10:01:28 +0500

    main.jsx

--- proficlub-crm/src/main.jsx
+++ proficlub-crm/src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import Register from './Register.jsx'
 import Attendance from './Attendance.jsx'
+import Survey from './Survey.jsx'
 
 const path = window.location.pathname
 const root = ReactDOM.createRoot(document.getElementById('root'))
@@ -11,6 +12,8 @@ if (path === '/register') {
   root.render(<Register />)
 } else if (path.startsWith('/attendance/')) {
   root.render(<Attendance />)
+} else if (path.startsWith('/survey/')) {
+  root.render(<Survey />)
 } else {
   root.render(<App />)
 }
